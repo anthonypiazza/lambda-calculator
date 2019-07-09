2452f6c98756574c194b7d28a674a9999a53c064
anthonypiazza <piazza.anthony.n@gmail.com>
2019-07-09 11:47:43 -0500

    Added yarn & styled dependency. created special buttons

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from 'styled-components';
 
-const SpecialButton = () => {
+const Button = styled.button`
+  border: 1px solid black;
+`
+
+
+const SpecialButton = props => {
   return (
-    <>
+    <Button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.special}
+    </Button>
   );
 };
+
+export default SpecialButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,22 @@
-import React from "react";
-
-//import any components needed
+import React, { useState } from "react";
+import { specials } from '../../../data';
+import SpecialButton from './SpecialButton';
 
 //Import your array data to from the provided data file
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [specialsState, setState]= useState();
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {specials.map(special => (
+        <SpecialButton special={special} id={special.index} />
+      ))}
     </div>
   );
 };
+
+export default Specials;
